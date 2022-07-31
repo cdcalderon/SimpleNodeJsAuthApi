01cd772c7b3589e6963c43cdad177fc13cbbdc30
Carlos Calderon <cdcalderon@gmail.com>
2022-07-30 20:45:43 -0500

    test validators

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,16 @@ const { Router } = require('express')
 const router = Router()
 
 // Create new user
-router.post('/new', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Create new user /new',
-    })
-})
+router.post(
+    '/new',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria').isLength({ min: 6 }),
+        validarCampos,
+    ],
+    crearUsuario
+)
 
 // Validate token
 router.get('/renew', (req, res) => {
