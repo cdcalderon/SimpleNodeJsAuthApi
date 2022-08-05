6b0a797ad476b5bdb3cce4f328cca0b03b09e8b6
Carlos Calderon <cdcalderon@gmail.com>
2022-08-05 14:20:19 -0500

    configure serving static files

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+
 const app = express()
 
 // CORS
 app.use(cors())
 
+// Public Directory
+app.use(express.static('public'))
+
 // read body parse
 app.use(express.json())
-
-require('dotenv').config()
 
 app.use('/api/auth', require('./routes/auth'))
 
