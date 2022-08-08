ed57057e7c6947518edf771d9cd30521d82c7939
Carlos Calderon <cdcalderon@gmail.com>
2022-08-08 15:01:03 -0500

    add validation for email

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,13 @@ const createUser = (req, res = response) => {
 
 const loginUser = (req, res = response) => {
     const errors = validationResult(req)
-    console.log(errors)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped(),
+        })
+    }
 
     return res.json({
         ok: true,
